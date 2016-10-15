ad4e4a01efe6d813266b288f4e79747531cf0874
William Bijansky <wb4@hawaii.edu>
2016-10-15 02:08:03 +0000

    add Student.java to Project.

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class student {
+  int age;
+  float gpa:
+  String firstName;
+  String lastName;
+  String major;
+
+  public Studen() {
+
+  }
+
+  public Student(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
